416beb0d86e3f3d61adb61ed437d7f88072dbc7e
Andrew Gallant <jamslam@gmail.com>
2015-03-22 19:32:02 -0400

    progress

--- src/btree.rs
+++ src/btree.rs
@@ -5,13 +5,14 @@ accessing any particular `Node` would count as a single disk IO. (Becuase we
 will stipulate that a `Node` fits on a single page.)
 */
 
+#![feature(collections)]
 #![allow(dead_code, unused_imports, unused_variables)]
 
 extern crate suffix;
 
 use std::borrow::{Cow, IntoCow};
 use std::fmt;
-use std::iter;
+use std::iter::{self, repeat};
 use suffix::SuffixTable;
 
 macro_rules! lg {
@@ -22,7 +23,6 @@ macro_rules! lg {
 }
 
 /// A suffix database represented with a btree.
-#[derive(Debug)]
 struct SufDB {
     /// All nodes in the tree.
     nodes: Vec<Node>,
@@ -56,7 +56,7 @@ struct Node {
     edges: Vec<KeyId>,
     /// Pointers to suffixes.
     suffixes: Vec<Suffix>,
-    prev: Option<NodeId>,
+    parent: Option<NodeId>,
     next: Option<NodeId>,
 }
 
@@ -70,7 +70,7 @@ impl ::std::ops::Deref for Document {
 }
 
 /// Represents a single suffix in a document.
-#[derive(Eq, Hash, PartialEq)]
+#[derive(Clone, Eq, Hash, PartialEq)]
 struct Suffix {
     /// Document index.
     docid: DocId,
@@ -86,10 +86,11 @@ impl Suffix {
 
 impl SufDB {
     fn new() -> SufDB {
-        SufDB::with_order(512)
+        SufDB::with_order(14)
     }
 
     fn with_order(order: usize) -> SufDB {
+        assert!(order > 2);
         SufDB {
             nodes: vec![Node::empty()],
             root: 0,
@@ -97,6 +98,11 @@ impl SufDB {
             order: order,
         }
     }
+
+    fn max_children(&self) -> usize { self.order }
+    fn max_keys(&self) -> usize { self.order - 1 }
+
+    fn is_root(&self, nid: NodeId) -> bool { self.root == nid }
 
     fn root(&mut self) -> &mut Node {
         &mut self.nodes[self.root]
@@ -141,14 +147,6 @@ impl SearchResult {
 }
 
 impl SufDB {
-    /// Find the first suffix `result` such that `needle >= result`.
-    ///
-    /// If no such suffix exists, then `None` is returned. (If the needle is
-    /// to be inserted, then it would become the first lexicographic suffix
-    /// in the tree.)
-    ///
-    /// When a suffix is found, this implies that the suffix that immediately
-    /// precedes `result` satisfies `needle > prev_result`.
     fn search<'d, 's, S>(&'d self, needle: S) -> Suffixes<'d, 's>
             where S: IntoCow<'s, str> {
         let needle = needle.into_cow();
@@ -174,30 +172,31 @@ impl SufDB {
 
     fn search_start_from(&self, nid: NodeId, needle: &str) -> SearchResult {
         let node = &self.nodes[nid];
-        let mut kid: Option<KeyId> = None;
-        for (i, suf) in node.suffixes.iter().enumerate() {
-            if needle <= self.suffix(suf) {
-                kid = Some(i);
-                break
-            }
-        }
-        let notfound = node.suffixes.len();
-        match (node.is_leaf(), kid) {
-            (true, None) => SearchResult::InsertAt(nid, notfound),
-            (true, Some(kid)) => {
+        match (node.is_leaf(), self.search_scan(nid, needle)) {
+            (true, Err(kid)) => SearchResult::InsertAt(nid, kid),
+            (true, Ok(kid)) => {
                 if self.suffix(&node.suffixes[kid]).starts_with(needle) {
                     SearchResult::Found(nid, kid)
                 } else {
                     SearchResult::InsertAt(nid, kid)
                 }
             }
-            (false, None) => {
-                self.search_start_from(node.edges[notfound], needle)
-            }
-            (false, Some(kid)) => {
+            (false, Err(kid)) | (false, Ok(kid)) => {
                 self.search_start_from(node.edges[kid], needle)
             }
         }
+    }
+
+    fn search_scan(&self, nid: NodeId, needle: &str) -> Result<KeyId, KeyId> {
+        let node = &self.nodes[nid];
+        let mut kid: Result<KeyId, KeyId> = Err(node.suffixes.len());
+        for (i, suf) in node.suffixes.iter().enumerate() {
+            if needle <= self.suffix(suf) {
+                kid = Ok(i);
+                break
+            }
+        }
+        kid
     }
 
     fn next_suffix(&self, nid: NodeId, kid: KeyId) -> Option<(NodeId, KeyId)> {
@@ -230,10 +229,14 @@ impl SufDB {
     fn insert_suffix(&mut self, suf: Suffix) {
         let (nid, kid) = self.search_insert_at(self.suffix(&suf));
         self.nodes[nid].suffixes.insert(kid, suf);
+        // if self.nodes[nid].suffixes.len() > self.max_keys() {
+            // let median = self.split(nid);
+        // }
     }
 
-    fn insert_at(&mut self, suf: Suffix, nid: NodeId) {
-    }
+    // fn split(&mut self, nid: NodeId) -> Suffix {
+        // let new = self.nodes[nid].split();
+    // }
 }
 
 impl Node {
@@ -241,13 +244,29 @@ impl Node {
         Node {
             edges: vec![],
             suffixes: vec![],
-            prev: None,
+            parent: None,
             next: None,
         }
     }
 
     fn is_leaf(&self) -> bool {
         self.edges.is_empty()
+    }
+
+    fn split(&mut self) -> Node {
+        let median = self.suffixes.len() / 2;
+        let split_sufs = self.suffixes.split_off(median);
+        let split_edges = if self.is_leaf() {
+            vec![]
+        } else {
+            self.edges.split_off(median)
+        };
+        Node {
+            edges: split_edges,
+            suffixes: split_sufs,
+            parent: self.parent,
+            next: None,
+        }
     }
 }
 
@@ -264,6 +283,41 @@ impl<'a, S> iter::Extend<S> for SufDB where S: IntoCow<'a, str> {
         for doc in docs {
             self.insert(doc);
         }
+    }
+}
+
+impl fmt::Debug for SufDB {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        macro_rules! w { ($($tt:tt)*) => (try!(writeln!(f, $($tt)*));); }
+        macro_rules! div {
+            () => (div!(79));
+            ($n:expr) => ({
+                w!("{}", repeat('-').take($n).collect::<String>());
+            });
+        }
+
+        div!();
+        w!("order: {}", self.order);
+        w!("#documents: {}", self.documents.len());
+        w!("#nodes: {}", self.nodes.len());
+        div!(39);
+        for doc in &self.documents { w!("{:?}", doc); }
+        div!(39);
+        for (i, n) in self.nodes.iter().enumerate() {
+            if i > 0 { div!(15); }
+            if self.root == i {
+                w!("id: {} (root)", i);
+            } else {
+                w!("id: {}", i);
+            };
+            w!("parent: {:?}", n.parent);
+            w!("next: {:?}", n.next);
+            w!("edges: {:?}", n.edges);
+            for (i, s) in n.suffixes.iter().enumerate() {
+                w!("{:4}: {:?}: {}", i, s, self.suffix(s));
+            }
+        }
+        div!(); Ok(())
     }
 }
 
@@ -317,12 +371,32 @@ mod tests {
 
     fn suf(d: DocId, s: SuffixId) -> Suffix { Suffix::new(d, s) }
 
-    fn assert_set_eq<T>(a: T, b: T)
-            where T: IntoIterator,
-                  <T as IntoIterator>::Item: Debug + Eq + Hash {
-        let s1: HashSet<<T as IntoIterator>::Item> = a.into_iter().collect();
-        let s2: HashSet<<T as IntoIterator>::Item> = b.into_iter().collect();
+    fn assert_set_eq<T, A, B>(a: A, b: B)
+            where T: Debug + Eq  + Hash,
+                  A: IntoIterator<Item=T>,
+                  B: IntoIterator<Item=T> {
+        let s1: HashSet<<A as IntoIterator>::Item> = a.into_iter().collect();
+        let s2: HashSet<<B as IntoIterator>::Item> = b.into_iter().collect();
         assert_eq!(s1, s2);
+    }
+
+    fn assert_search(db: &SufDB, search: &str, sufs: Vec<(DocId, SuffixId)>) {
+        assert_set_eq(db.search(search).map(|s| s.clone()),
+                      sufs.into_iter().map(|(d, s)| suf(d, s)));
+    }
+
+    #[test]
+    fn search_empty_db() {
+        let db = SufDB::new();
+        assert!(!db.contains(""));
+        assert!(!db.contains("a"));
+    }
+
+    #[test]
+    fn search_one_letter() {
+        let db = createdb(vec!["a"]);
+        assert!(db.contains("a"));
+        assert_search(&db, "a", vec![(0, 0)]);
     }
 
     #[test]
@@ -349,22 +423,21 @@ mod tests {
     #[test]
     fn search_two_similar() {
         let db = createdb(vec!["maple", "apple"]);
-        let results: Vec<_> = db.search("ple").collect();
-        assert_set_eq(results, vec![&suf(0, 2), &suf(1, 2)]);
+        assert_search(&db, "ple", vec![(0, 2), (1, 2)]);
     }
 
     #[test]
     fn search_two_snowmen() {
         let db = createdb(vec!["☃abc☃", "apple"]);
-        let results: Vec<_> = db.search("☃").collect();
-        assert_set_eq(results, vec![&suf(0, 0), &suf(0, 6)]);
+        assert_search(&db, "☃", vec![(0, 0), (0, 6)]);
     }
 
     #[test]
-    #[ignore]
     fn scratch() {
         let mut db = SufDB::new();
         db.insert("banana");
+        db.insert("apple");
+        db.insert("orange");
         lg!("{:?}", db);
     }
 }
